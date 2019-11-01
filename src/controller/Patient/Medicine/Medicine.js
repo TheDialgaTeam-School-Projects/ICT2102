3187e2ffe8a03a95915d35c05d08254b962f61d7
Glen Goh <1802505@sit.singaporetech.edu.sg>
2019-11-01 10:29:03 +0800

    medicine (still cannot fetch data)

--- src/controller/Patient/Medicine/Medicine.js
+++ src/controller/Patient/Medicine/Medicine.js
@@ -3,5 +3,26 @@ import {Controller} from '../../Controller';
 export class MedicineController extends Controller {
   constructor(view) {
     super(view);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.willFocus = this.willFocus.bind(this);
   }
+
+  componentDidMount() {
+    this.state = {medicineModel: null};
+  }
+
+  async willFocus() {
+      try {
+        this.state = {
+          medicineModel: await DeviceCacheManagement.getPatientModelFromCache(
+            true,
+          ),
+        };
+      } catch (e) {
+        await AsyncStorage.removeItem('patientInformation');
+        this.state = {patientModel: null};
+        this.navigate('RegisterPatient');
+      }
+    }
+
 }
--- src/view/Patient/Medicine/Medicine.js
+++ src/view/Patient/Medicine/Medicine.js
@@ -1,3 +1,4 @@
+import firestore from '@react-native-firebase/firestore';
 import React, {Component} from 'react';
 import {Col, Grid, Row} from 'react-native-easy-grid';
 import {
@@ -12,6 +13,10 @@ import {
   Text,
   Title,
   View,
+  Header,
+  Card,
+  CardItem,
+  Body
 } from 'native-base';
 import {GlobalCss} from '../../../css/GlobalCss';
 import {HeaderComponent} from '../../Component/Header';
@@ -21,12 +26,49 @@ export class MedicineView extends Component {
   constructor(props) {
     super(props);
     this.controller = new MedicineController(this);
+    this.state = {
+
+        test: [],
+    };
   }
 
+  async willFocus() {
+  const query = await firestore()
+      .collection('patients')
+      .get()
+      .then(function(querySnapshot) {
+      console.log("SnapShot"+querySnapshot);
+        querySnapshot.forEach(function(doc) {
+            var data = doc.data();
+            //console.log(data.Medicine);
+            this.setState({ test: data.Medicine });
+            })
+        });
+    console.log(this.state.test);
+  }
+
+  componentDidMount()
+  {
+  this.willFocus();
+  }
   render() {
     return (
       <Container>
         <HeaderComponent headerTitle="MEDICINE" {...this.props} />
+            <Content padder>
+              <Card>
+                <CardItem header bordered>
+                  <Text style={{fontWeight: 'bold', fontSize: 32}}>Medicine Taken By Patient</Text>
+                </CardItem>
+                <CardItem bordered>
+                  <Body>
+                    <Text>
+                    {this.state.test}
+                    </Text>
+                  </Body>
+                </CardItem>
+              </Card>
+            </Content>
       </Container>
     );
   }
